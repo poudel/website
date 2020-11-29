b32bc752cdbd14c276b60a8aa6218e8dca02b76b
keshab <github.poudel@self.keshab.net>
2020-11-29 01:20:16 +0100

    batman

---
+++ Makefile
@@ -0,0 +1,29 @@
+build: website.el
+	@echo "Building the website"
+	emacs --batch -q -l website.el -f org-publish-all
+
+
+clean:
+	@echo "Cleaning up.."
+	@rm -rvf *.elc
+	@rm -rvf public
+	@rm -rvf ~/.org-timestamps/*
+
+
+pub: 
+	@echo "Deleting old..."
+	rm -rf public
+	mkdir public
+	git worktree prune
+	rm -rf .git/worktrees/public
+
+	@echo "Checking out gh-pages branch into public"
+	git worktree add -B gh-pages public origin/gh-pages
+
+	@echo "Removing existing files"
+	rm -rf public/*
+
+	$(MAKE) build
+
+	@echo "Updating gh-pages branch"
+	cd public && git add --all && git commit -m "publishing to gh-pages"
---
+++ website.el
@@ -0,0 +1,44 @@
+(require 'org)
+
+(setq
+ org-publish-project-alist
+ '(("home"
+    :base-directory "src"
+    :base-extension "org"
+    :publishing-directory "public"
+    :recursive nil
+    :publishing-function org-html-publish-to-html)
+
+   ("static"
+    :base-directory "src"
+    :base-extension "css\\|js\\|png\\|svg"
+    :publishing-directory "public"
+    :publishing-function org-publish-attachment
+    :recursive t)
+
+   ("blog"
+    :base-directory "src/blog"
+    :base-extension "org"
+    :publishing-directory "public/blog"
+    :recursive t
+    :publishing-function org-html-publish-to-html
+
+    :section-numbers nil
+    :with-creator t
+    :with-date t
+    :with-toc nil
+
+    :html-head nil
+    :html-head-include-default-style nil
+    :html-head-extra "<link rel='stylesheet' href='../static/index.css' />"
+
+    :auto-sitemap t
+    :sitemap-title "keshab paudel's blog"
+    :sitemap-filename "index.org"
+    :sitemap-style list
+    :sitemap-sort-files anti-chronologically
+    )
+
+   ("website"
+    :components ("blog" "home" "static"))
+   ))
